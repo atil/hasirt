b79f6231f77bf20b8953adae5d45c8001f25cc30
atil <hyperion91@gmail.com>
2017-02-16 23:27:04 +0300

    Progress on gameplay loop;

--- wave.js
+++ wave.js
@@ -1,5 +1,4 @@
 function preloadWave(game) {
-
     game.load.image('left', 'assets/left.png');
     game.load.image('right', 'assets/right.png');
     game.load.image('up', 'assets/up.png');
@@ -7,25 +6,59 @@ function preloadWave(game) {
 }
 
 function initWave(game) {
-
     var wave = game.add.sprite(game.rect.x, game.rect.y, 'blue');
     wave.anchor.setTo(0.5, 0.5);
-
-    var group = game.add.group();
-    var left;
-    for (var i = 0; i < 5; i++) {
-        left = group.create(0, 0, 'left');
+    wave.sequence = [
+        'up', 'down', 'left', 'right'
+    ]
+    wave.result = {
+        continue : 1,
+        complete : 2,
+        fail : 3,
     }
-    group.align(game.rect.width - 50, left.height * 2 , left.width + 10, left.height, Phaser.BOTTOM_RIGHT);
 
-    group.x = game.rect.width * 0.5;
-    group.y = game.rect.height * 0.3;
+    wave.reAlign = function () {
+        wave.group.align(game.rect.width - 50, sprite.height * 2 , sprite.width + 10, sprite.height);
+    }
+
+
+    wave.group = game.add.group();
+    var sprite;
+    for (var i = 0; i < wave.sequence.length; i++) {
+        sprite = wave.group.create(0, 0, wave.sequence[i]);
+    }
+    
+    wave.reAlign();
+
+    wave.group.x = game.rect.width * 0.5;
+    wave.group.y = game.rect.height * 0.3;
+
+    wave.current = 0;
 
     return wave;
 }
 
-function updateWave(wave, dt) {
+function processKeyPress(wave, key) {
+    if (key == wave.sequence[wave.current]) {
+        wave.current++;
+        console.log(" 1 " + wave.continue)
+        return wave.result.continue;
+    } else if (wave.current == wave.sequence.length) {
+        wave.sequence.push(key);
+        wave.group.create(0, 0, key);
+        wave.reAlign();
+        wave.current = 0;
+        console.log(" 2 " + wave.complete)
+        return wave.result.complete;
+    } else {
+        wave.current = 0;
+        console.log(" 3 " + wave.fail)
+        return wave.result.fail;
+    }
+}
 
-    wave.position.x += 15 * dt;
+function updateWave(wave, dir, dt) {
+    var spd = (dir ? 1 : -1) * 15;
+    wave.position.x += spd * dt;
 
 }
